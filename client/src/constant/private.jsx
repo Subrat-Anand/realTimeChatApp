a56e6535cbdd9ddba197b04a265ba6e7728c5737
ApnaCollageYoutube <anandsinha975@gmail.com>
2025-06-21 16:47:03 +0530

    Add previously ignored constants folder

---
+++ client/src/constant/private.jsx
@@ -0,0 +1,4 @@
+export const AUTH_URL = "http://localhost:8000/api/auth"
+export const GET_USER = "http://localhost:8000/api/user"
+export const BASE_URL = "http://localhost:8000/api"
+export const Back_end = "http://localhost:8000"
